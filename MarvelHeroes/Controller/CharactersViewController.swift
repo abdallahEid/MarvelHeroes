cd8eb7d99fa4a3b15f66416d511abd92feb57b7a
abdallahEid <abdallaheidm@gmail.com>
2019-08-01 22:12:07 +0200

    Design: Implementing characters tableview screen

--- MarvelHeroes/Controller/CharactersViewController.swift
+++ MarvelHeroes/Controller/CharactersViewController.swift
@@ -1,21 +1,29 @@
 //
-//  CharacterViewController.swift
+//  CharactersViewController.swift
 //  MarvelHeroes
 //
 //  Created by Abdallah Eid on 8/1/19.
 //  Copyright © 2019 Abdallah Eid. All rights reserved.
 //
 
+/* NOTE: There is an extension to handle table view protocols (delegates and data sources) in path: /Extensions/CharactersViewController+TableView
+ */
+
 import UIKit
 
-class CharacterViewController: UIViewController {
+class CharactersViewController: UIViewController {
 
+    // MARK: IBOutlets & Properties
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     // MARK: ViewController LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavbarLogo()
+        configureTable()
     }
     
     // MARK: IBAction & Functions
@@ -28,5 +36,12 @@ class CharacterViewController: UIViewController {
         imageView.image = image
         
         navigationItem.titleView = imageView
+    }
+    
+    func configureTable(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.register(CharacterCell.nib, forCellReuseIdentifier: CharacterCell.reuseIdentifier)
     }
 }
---
+++ MarvelHeroes/Extensions/CharactersViewController+TableView.swift
@@ -0,0 +1,33 @@
+//
+//  CharactersViewController+TableView.swift
+//  MarvelHeroes
+//
+//  Created by Abdallah Eid on 8/1/19.
+//  Copyright © 2019 Abdallah Eid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension CharactersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CharacterCell.reuseIdentifier ) as! CharacterCell
+        
+        cell.characterName.text = "Iron Man"
+        cell.characterImageView.image = UIImage(named: "ironman")
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+}
+
+extension CharactersViewController: UITableViewDelegate {
+
+}
